41bbb8e6c375ad45744d831ebcbefe2af448ad54
Kamran Mushtaq <kaamipresents@gmail.com>
2026-02-24 02:27:03 +0500

    Unit Test Problem Resolved

--- Lecture 5 - Unit Tests/Practice/problem1/problem1.py
+++ Lecture 5 - Unit Tests/Practice/problem1/problem1.py
@@ -1,10 +1,6 @@
 import math
 
 def calculate_factorial(value):
-    """
-    Handles the conversion to integer, validation, 
-    and factorial calculation.
-    """
     try:
         # 1. Convert the passed input to an integer
         number = int(value)
@@ -15,15 +11,18 @@ def calculate_factorial(value):
         
         # 3. Use the math library for the calculation
         result = math.factorial(number)
-        return f"The factorial of {number} is {result}"
+        return result
             
     except ValueError:
         return "Error: Invalid input! Please provide a valid whole number."
     except Exception as e:
         return f"An unexpected error occurred: {e}"
 
-if __name__ == "__main__":
-    # --- Execution ---
+def main():
     user_provided_input = input("Enter a non-negative integer: ")
     output = calculate_factorial(user_provided_input)
     print(output)
+
+if __name__ == "__main__":
+    # --- Execution ---
+    main()
--- Lecture 5 - Unit Tests/Practice/problem1/test-problem1.py
+++ Lecture 5 - Unit Tests/Practice/problem1/test-problem1.py
@@ -1,6 +1,15 @@
 from problem1 import calculate_factorial
 
 def test_calculate_factorial():
+    assert calculate_factorial(-2) == 1
+    assert calculate_factorial(0) == 1
     assert calculate_factorial(1) == 1
     assert calculate_factorial(2) == 2
     assert calculate_factorial(3) == 6
+    print("All tests passed!")
+
+def main():
+    test_calculate_factorial()
+
+if __name__ == "__main__":
+    main()
