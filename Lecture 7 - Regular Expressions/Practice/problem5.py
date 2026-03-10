22a02f76faa01072c0ccbe8fbc210c0aef234de3
Kamran Mushtaq <kaamipresents@gmail.com>
2026-03-10 16:49:31 +0500

    Lecture 7 - Practice Day 1

--- Lecture 7 - Regular Expressions/Practice/problem5.py
+++ Lecture 7 - Regular Expressions/Practice/problem5.py
@@ -5,7 +5,7 @@ import re
 text = "Contact me at kami@gmail.com"
 
 # Regular expression to find an email pattern in the text
-pattern = r'\w+@\w+\.\w+'
+pattern = r'\w+@\w+\.\w+' 
 # Search for the pattern in the text
 match = re.search(pattern, text)
 if match:
